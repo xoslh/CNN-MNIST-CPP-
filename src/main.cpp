3469f746473bc043de72149c24c407204ebd69fa
xoslh <3070026867@qq.com>
2023-05-21 13:10:07 +0000

    by yxy on 2023/5/21

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,22 @@
 using namespace std;
 int main()
 {
-	vector<case_t> cases = read_test_cases();
+	vector<case_t> cases = read_train_cases();
 	
 	Model M;
-	M.add_conv( 1, 5, 8, {28, 28, 1} );
+	M.add_conv( 1, 5, 10, {28, 28, 1} );
 	M.add_relu( M.output_size() );
 	M.add_pool( 2, 2, M.output_size() );
-	M.add_conv( 1, 3, 10, M.output_size() );
+	M.add_conv( 1, 3, 12, M.output_size() );
 	M.add_relu( M.output_size() );
 	M.add_pool( 2, 2, M.output_size() );
-	M.add_fc(M.output_size(),10);
-
+	M.add_fc( M.output_size(), 10 );
+ 
 	float sum_err = 0;
 	int cnt = 0;
 	float acc=0;
 	cout<<"start training..."<<endl;
-	int T=2;
+	int T=4;
 	while(T--)
 		for ( case_t& t : cases )
 		{
@@ -29,9 +29,20 @@ int main()
 			acc += t.out( M.predict(), 0, 0 ) > 0.5 ? 1.0f : 0.0f;
 			if ( cnt % 1000 == 0 )
 			{
-				cout << "cases: " << cnt << " err=" << sum_err/cnt << " acc=" << acc/10.0 << "%" << endl;
+				cout << "cases: " << cnt << " err=" << sum_err/cnt << " acc=" << acc/1000.0 << endl;
 				acc=0;
 			}
 		}
+	
+	cout << "start testing ..." << endl;
+	cases = read_test_cases();
+	acc = sum_err = 0;
+	for( case_t& t : cases )
+	{
+		M.forward( t.data ); 
+		acc += t.out( M.predict(), 0, 0 ) > 0.5 ? 1.0f : 0.0f;
+	}
+	cout << "testing results: acc="<< acc/(float)(cases.size()) <<endl;
+
 	return 0;
 }
--- src/read_func.h
+++ src/read_func.h
@@ -34,12 +34,47 @@ uint8_t* read_file( const char* szFile )
 	return buffer;
 }
 
-vector<case_t> read_test_cases()
+vector<case_t> read_train_cases()
 {
 	vector<case_t> cases;
 
 	uint8_t* train_image = read_file( "train-images.idx3-ubyte" );
 	uint8_t* train_labels = read_file( "train-labels.idx1-ubyte" );
+
+	uint32_t case_count = byteswap_uint32( *(uint32_t*)(train_image + 4) );
+
+	for ( int i = 0; i < case_count; i++ )
+	{
+		case_t c
+		{
+			tensor_t<float>( 28, 28, 1 ),
+				tensor_t<float>( 10, 1, 1 )
+				};
+
+		uint8_t* img = train_image + 16 + i * (28 * 28);
+		uint8_t* label = train_labels + 8 + i;
+
+		for ( int x = 0; x < 28; x++ )
+			for ( int y = 0; y < 28; y++ )
+				c.data( x, y, 0 ) = img[x + y * 28] / 255.f;
+
+		for ( int b = 0; b < 10; b++ )
+			c.out( b, 0, 0 ) = *label == b ? 1.0f : 0.0f;
+
+		cases.push_back( c );
+	}
+	delete[] train_image;
+	delete[] train_labels;
+
+	return cases;
+}
+
+vector<case_t> read_test_cases()
+{
+	vector<case_t> cases;
+
+	uint8_t* train_image = read_file( "t10k-images.idx3-ubyte" );
+	uint8_t* train_labels = read_file( "t10k-labels.idx1-ubyte" );
 
 	uint32_t case_count = byteswap_uint32( *(uint32_t*)(train_image + 4) );
 
