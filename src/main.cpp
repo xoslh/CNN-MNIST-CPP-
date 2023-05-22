8c18b820cfb42873cd852032941b7145731e79fb
xoslh <3070026867@qq.com>
2023-05-22 15:58:09 +0000

    test change

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,24 @@ int main()
 	vector<case_t> cases = read_train_cases();
 	
 	Model M;
-	M.add_conv( 1, 5, 10, {28, 28, 1} );
+	M.add_conv( 1, 5, 8, {28, 28, 1} );
 	M.add_relu( M.output_size() );
 	M.add_pool( 2, 2, M.output_size() );
-	M.add_conv( 1, 3, 12, M.output_size() );
+
+	M.add_conv( 1, 2, 10, M.output_size() );
 	M.add_relu( M.output_size() );
 	M.add_pool( 2, 2, M.output_size() );
+
+	M.add_conv( 1, 2, 12, M.output_size() );
+	M.add_relu( M.output_size() );
+
 	M.add_fc( M.output_size(), 10 );
  
 	float sum_err = 0;
 	int cnt = 0;
 	float acc=0;
 	cout<<"start training..."<<endl;
-	int T=2;
+	int T=10;
 	while(T--)
 		for ( case_t& t : cases )
 		{
